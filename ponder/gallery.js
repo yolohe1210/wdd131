e6d6ef261d328b6d1b827db259eed49ca3ab35f3
Yin He <yheus25@gmail.com>
2026-01-28 12:48:42 -0700

    W04 Ponder: Responsive Gallery with Modals

---
+++ ponder/gallery.js
@@ -0,0 +1,31 @@
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('sm', 'full');
+    modalImage.src = full;
+    modalImage.alt = alt;
+    modal.showModal();
+// Code to show modal  - Use event parameter 'e'   
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
